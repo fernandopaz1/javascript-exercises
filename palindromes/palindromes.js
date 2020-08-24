37b73a81df3a38e88e6346bdf978ba39d979c415
Fernando Paz <ferpaz9292@gmail.com>
2020-08-24 19:06:29 -0300

    Resolucion de palindromos

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function() {
+const palindromes = function(s) {
+    console.log("\n\n");
+    let splited= s.split('');
+   
+    let filtered= splited.filter(letter => {
+        let inLower = letter.toLowerCase()
+        if(inLower>="a" && inLower<="z") return true;
+        else return false;
+    })
 
+    let fLower= filtered.map(letter => letter.toLowerCase());     
+    
+    while(fLower.length>=2){
+        let largo=fLower.length;
+        if(fLower[0]!=fLower[largo-1]) return false;
+        fLower=fLower.slice(1,-1);
+    }
+    return true;
 }
 
 module.exports = palindromes
